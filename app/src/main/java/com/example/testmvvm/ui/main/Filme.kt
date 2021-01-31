7645064af25a34d49423c7eba268ba27e984da8f
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-01-30 23:47:52 -0300

    mainRepository adicionado

---
+++ app/src/main/java/com/example/testmvvm/ui/main/Filme.kt
@@ -0,0 +1,6 @@
+package com.example.testmvvm.ui.main
+
+data class Filme(
+    val id: Int,
+    val titulo: String
+)
---
+++ app/src/main/java/com/example/testmvvm/ui/main/MainRepository.kt
@@ -0,0 +1,15 @@
+package com.example.testmvvm.ui.main
+
+class MainRepository {
+
+    fun getFilmes(callback: (filmes : List<Filme>) -> Unit) {
+         Thread(Runnable {
+            Thread.sleep(3000)
+            callback.invoke(
+                listOf(
+                Filme(1,"Título 01"),
+                Filme(1,"Título 02")
+            ))
+        })
+    }
+}
